e7f30b226838b2cc07f5e7ddeb180f01d57637e8
chajk811 <chajk811@gmail.com>
2020-01-15 15:01:41 +0900

    field name

--- myapp/item/serializers.py
+++ myapp/item/serializers.py
@@ -2,16 +2,16 @@ from rest_framework import serializers
 from .models import Item
 
 class ItemSerializer(serializers.ModelSerializer):
-    imageUrl = serializers.CharField(source='imageId')
+    imgUrl = serializers.CharField(source='imageId')
 
     class Meta:
         model = Item
-        fields = ('id', 'imageUrl', 'name', 'price', 'ingredients', 'monthlySales',)
+        fields = ('id', 'imgUrl', 'name', 'price', 'ingredients', 'monthlySales',)
 
 
 class RecommendSerializer(serializers.ModelSerializer):
-    imageUrl = serializers.CharField(source='imageId')
+    imgUrl = serializers.CharField(source='imageId')
 
     class Meta:
         model = Item
-        fields = ('id', 'imageUrl', 'name', 'price',)
+        fields = ('id', 'imgUrl', 'name', 'price',)
